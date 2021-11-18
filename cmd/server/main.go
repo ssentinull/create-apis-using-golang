150394aac0290f8b2a95e7cf58597d75b69e7742
IbnuAhsani <ibnu.muhari@gmail.com>
2021-11-18 16:43:14 +0700

    feat: setup logrus param values in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/sirupsen/logrus"
 )
+
+// initialize logger configurations
+func initLogger() {
+	logrus.SetFormatter(&logrus.TextFormatter{
+		ForceColors:     true,
+		DisableSorting:  true,
+		DisableColors:   false,
+		FullTimestamp:   true,
+		TimestampFormat: "15:04:05 02-01-2006",
+	})
+
+	logrus.SetOutput(os.Stdout)
+	logrus.SetReportCaller(true)
+	logrus.SetLevel(logrus.ErrorLevel)
+}
+
+// run initLogger() before running main()
+func init() {
+	initLogger()
+}
 
 func main() {
 	e := echo.New()
@@ -20,5 +41,5 @@ func main() {
 		WriteTimeout: 2 * time.Minute,
 	}
 
-	log.Fatal(e.StartServer(s))
+	logrus.Fatal(e.StartServer(s))
 }
