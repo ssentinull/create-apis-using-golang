b80c7c2af13b26cbcbaea26ae660c8e302772900
IbnuAhsani <ibnu.muhari@gmail.com>
2021-11-23 11:42:26 +0700

    feat: book usecase

---
+++ pkg/book/usecase/book_usecase.go
@@ -0,0 +1,52 @@
+package usecase
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/sirupsen/logrus"
+	"github.com/ssentinull/create-apis-using-golang/pkg/model"
+)
+
+type bookUsecase struct {
+	bookRepo model.BookRepository
+}
+
+func NewBookUsecase(br model.BookRepository) model.BookUsecase {
+	return &bookUsecase{bookRepo: br}
+}
+
+func (bu *bookUsecase) GetBookByID(ctx context.Context, ID int64) (model.Book, error) {
+	book, err := bu.bookRepo.ReadBookByID(ctx, ID)
+	if err != nil {
+		c, err := json.Marshal(ctx)
+		if err != nil {
+			logrus.Error(err)
+		}
+
+		logrus.WithFields(logrus.Fields{
+			"ctx": c,
+			"ID":  ID,
+		}).Error(err)
+
+		return model.Book{}, err
+	}
+
+	return book, nil
+}
+
+func (bu *bookUsecase) GetBooks(ctx context.Context) ([]model.Book, error) {
+	books, err := bu.bookRepo.ReadBooks(ctx)
+	if err != nil {
+		c, err := json.Marshal(ctx)
+		if err != nil {
+			logrus.Error(err)
+		}
+
+		logrus.WithField("ctx", c).Error(err)
+
+		return nil, err
+	}
+
+	return books, nil
+}
