fa32ec0d3a96c3737e6deb89f3a3de6ba5ec961f
ibnu.ahsani <ibnu.ahsani@kumparan.com>
2022-07-10 15:14:32 +0700

    feat: utils dump func

---
+++ pkg/utils/dump.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+)
+
+// Dump dump i to json
+func Dump(i interface{}) string {
+	bt, err := json.Marshal(i)
+	if err != nil {
+		logrus.Error(err)
+	}
+
+	return string(bt)
+}
